a0c75f802b08948460cdc262a3d479275a71113e
Rohit Kodam <rohitkodam4@gmail.com>
2023-08-31 07:26:52 +0530

    switched to waitlist form

--- src/components/landing/HeaderLanding.tsx
+++ src/components/landing/HeaderLanding.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SigninBtn from '@/components/landing/SigninBtn';
+import axios from '@/lib/axiosClient';
+import Input from '@/components/ui/forms/input';
 
 interface HeaderLandingProps {}
 
 export const HeaderLanding: React.FC<HeaderLandingProps> = ({}) => {
+  const [email, setEmail] = useState('');
+  const onSubmitHandler = () => {
+    if (email === '' || !email.includes('@')) return;
+    axios
+      .post(
+        `https://api-v1.defi-os.com/waitlist/jobs?email=${email}&wl_type=jobs`
+      )
+      .then((res) => {
+        setEmail('');
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="z-[20] mt-16 flex w-screen flex-col items-center">
       <div className="text-center text-5xl font-black text-white 2xl:text-6xl">
@@ -18,7 +32,23 @@ export const HeaderLanding: React.FC<HeaderLandingProps> = ({}) => {
         incentivize any open source project. <br />
       </div>
       <div className="mt-12">
-        <SigninBtn />
+        {/* <SigninBtn /> */}
+        <div className="flex gap-4">
+          <Input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-[30rem]"
+            inputClassName="!border-body drop-shadow-xl"
+          />
+          <div
+            className="text-newdark flex w-fit cursor-pointer items-center justify-center rounded-full bg-primary px-8 text-sm font-semibold xl:text-base 3xl:text-lg"
+            onClick={onSubmitHandler}
+          >
+            Join Waitlist
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/landing/LandingPreFooter.tsx
+++ src/components/landing/LandingPreFooter.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '@/assets/images/logo.png';
 import Image from '@/components/ui/image';
 import SigninBtn from '@/components/landing/SigninBtn';
+import axios from '@/lib/axiosClient';
+import Input from '@/components/ui/forms/input';
 
 interface LandingPreFooterProps {}
 
 export const LandingPreFooter: React.FC<LandingPreFooterProps> = ({}) => {
+  const [email, setEmail] = useState('');
+  const onSubmitHandler = () => {
+    if (email === '' || !email.includes('@')) return;
+    axios
+      .post(
+        `https://api-v1.defi-os.com/waitlist/jobs?email=${email}&wl_type=jobs`
+      )
+      .then((res) => {
+        setEmail('');
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="flex w-screen flex-col items-center justify-center gap-6 bg-[#060606] py-20">
       <div className="relative h-20 w-20 overflow-hidden rounded-full xl:h-24 xl:w-24 3xl:h-28 3xl:w-28">
@@ -24,7 +38,23 @@ export const LandingPreFooter: React.FC<LandingPreFooterProps> = ({}) => {
         join defiOS, earn, build and help open source win.
       </div>
       <div className="mt-12">
-        <SigninBtn />
+        {/* <SigninBtn /> */}
+        <div className="flex gap-4">
+          <Input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-[30rem]"
+            inputClassName="!border-body drop-shadow-xl"
+          />
+          <div
+            className="text-newdark flex w-fit cursor-pointer items-center justify-center rounded-full bg-primary px-8 text-sm font-semibold xl:text-base 3xl:text-lg"
+            onClick={onSubmitHandler}
+          >
+            Join Waitlist
+          </div>
+        </div>
       </div>
     </div>
   );
